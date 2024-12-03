301e4428f0e7d581c3a83bc7ce8aea9ef07a3ba3
patekfilip <filip.lakic.sg@gmail.com>
2024-12-03 10:45:09 +0100

    feat: solve second puzzle day 3

--- src/day3/Day3Puzzle.java
+++ src/day3/Day3Puzzle.java
@@ -11,12 +11,24 @@ public class Day3Puzzle {
     public void solve() {
         try {
             String content = new String(Files.readAllBytes(Paths.get("./src/day3/input")));
-            Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+            Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
             Matcher matcher = pattern.matcher(content);
 
+            boolean isDo = true;
             List<Integer> results = new ArrayList<>();
             while (matcher.find()) {
-                results.add(Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2)));
+                if (matcher.group().equals("do()")) {
+                    isDo = true;
+                    continue;
+                }
+                if (matcher.group().equals("don't()")) {
+                    isDo = false;
+                    continue;
+                }
+
+                if (isDo) {
+                    results.add(Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2)));
+                }
             }
 
             System.out.println("Sum of Multiplications: ");
